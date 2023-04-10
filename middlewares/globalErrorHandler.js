2e8e43ae449b84f1a7f14e2de44abe9e654e5641
nimabht <nimabeheshtaein99@gmail.com>
2023-04-10 13:16:15 +0330

    global error handler created.

---
+++ middlewares/globalErrorHandler.js
@@ -0,0 +1,51 @@
+const winston = require("winston");
+require("winston-mongodb");
+
+const options = {
+  file: {
+    level: "info",
+    filename: "info.log",
+    handleExceptions: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 10,
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json(),
+      winston.format.prettyPrint()
+    ),
+  },
+  console: {
+    level: "debug",
+    handleExceptions: true,
+    format: winston.format.combine(winston.format.simple()),
+  },
+  dataBase: {
+    level: "error",
+    db: "mongodb://localhost/HW16-employee",
+  },
+};
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console),
+    new winston.transports.MongoDB(options.dataBase),
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
+
+module.exports = (Error, req, res, next) => {
+  if (
+    !Error.statusCode ||
+    Error.statusCode.toString().startsWith("5")
+  ) {
+    logger.error(Error.message, { metadata: Error });
+  }
+  res.status(Error.statusCode || 500);
+  res.send({
+    error: Error.status || "error",
+    message: Error.statusCode.toString().startsWith("5")
+      ? "Internal Server Error"
+      : Error.message,
+  });
+};
