eb83c5dfef94fb56dbbbc94243dfc3243330b48f
nimabht <nimabeheshtaein99@gmail.com>
2023-04-12 00:35:18 +0330

    delete Company in client side added

---
+++ public/js/company-edit/deleteCompany.js
@@ -0,0 +1,30 @@
+const deleteCompany = () => {
+  let polipop = new Polipop("updateSection", {
+    layout: "popups",
+    insert: "before",
+    pool: 5,
+    life: 3000,
+    progressbar: true,
+  });
+  let url = window.location.href;
+  let id = url.match(/edit\/(.*)/)[1];
+  axios
+    .delete(`/api/company/${id}`)
+    .then((_response) => {
+      polipop.add({
+        type: "info",
+        title: "Deleted!",
+        content: "Company deleted successfully!",
+      });
+      setTimeout(() => {
+        window.location.href = `http://localhost:1010/companies`;
+      }, 3000);
+    })
+    .catch((_error) => {
+      polipop.add({
+        type: "error",
+        title: "Error",
+        content: "Something is wrong!!!",
+      });
+    });
+};
