f15b0b1ddfbe605367f33e7dcefb82d714b9ec25
nimabht <nimabeheshtaein99@gmail.com>
2023-04-12 12:15:20 +0330

    getting all employees added to route

--- public/js/employees/employees.js
+++ public/js/employees/employees.js
@@ -1,6 +1,6 @@
 $(() => {
   axios
-    .get(`/api/employee`)
+    .get(`/api/employee?page=1`)
     .then((response) => {
       const employees = response.data.data;
       renderContainer(employees, response.data.total, 1);
